44960878343f6d00592af53b733b6ae904179e15
mahmud <mahmudarif1175@gmail.com>
2019-07-17 16:39:35 +0600

    enable css modules and add .env file

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use strict';
 
 const fs = require('fs');
@@ -401,6 +402,8 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
